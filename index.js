efa36f2335e3d57e44867dc828299e41c896ee66
mihir(faux__) <karbelkar.mihir@gmail.com>
2020-04-02 19:23:13 +0530

    added files

---
+++ index.js
@@ -0,0 +1,48 @@
+var app = require("express")();
+var express = require("express");
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
+var port = process.env.PORT || 8080;
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.use(express.static("public"));
+
+let todolist = [];
+io.on("connection", function(socket) {
+  console.log("a user connected");
+  socket.on("disconnect", function() {
+    console.log("user disconnected");
+  });
+
+  socket.emit("updateTask", todolist);
+
+  // Adds task on the todolist
+  socket.on("addTask", function(task) {
+    todolist.push(task); // Add task to server todolist array
+
+    // Get the index position of task in array - to give kind of id
+    index = todolist.length - 1;
+
+    // console.log(task); // Debug task
+    // console.log(index); // Debug index
+
+    // Send task to all users in real-time
+    socket.broadcast.emit("updateTask", todolist);
+    // console.log(todolist); // Debug
+  });
+
+  // Delete tasks
+  socket.on("deleteTask", function(index) {
+    // Deletes task from the server todolist array
+    todolist.splice(index, 1);
+
+    // Updates todolist of all users in real-time - refresh index
+    socket.broadcast.emit("updateTask", todolist);
+  });
+});
+
+app.listen(80, "0.0.0.0", function() {
+  console.log("listening on *:4001");
+});
---
+++ public/precache-manifest.d3e667b7a1d11b76ffc273a09fef964f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "416071c3b8998c3b88092e01e034f5e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ccedf70e0c76db021c9",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "25754f3de0a51d95a103",
+    "url": "/static/css/main.5fcd1417.chunk.css"
+  },
+  {
+    "revision": "4ccedf70e0c76db021c9",
+    "url": "/static/js/2.2da12c6e.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.2da12c6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25754f3de0a51d95a103",
+    "url": "/static/js/main.53e2c730.chunk.js"
+  },
+  {
+    "revision": "252f969779de0734efb3",
+    "url": "/static/js/runtime-main.75211130.js"
+  }
+]);
